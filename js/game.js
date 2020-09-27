6f0a64fbdcc7e3a47cd07621c85b27a961e59703
Trifle22 <xromchenkov99@gmail.com>
2020-09-27 13:21:07 +0300

    +bgr

---
+++ js/game.js
@@ -0,0 +1,21 @@
+let cvs = document.getElementById('canvas');
+let ctx = cvs.getContext('2d');
+
+let bird = new Image();
+let bg = new Image();
+let fg = new Image();
+let pipeUp = new Image();
+let pipeBottom = new Image();
+
+bird.src = 'img/bird.png';
+bg.src = 'img/bg.png';
+fg.src = 'img/fg.png';
+pipeUp.src = 'img/pipeUp.png';
+pipeBottom.src = 'img/pipeBottom.png';
+
+
+function draw() {
+  ctx.drawImage(bg, 0, 0);
+}
+
+pipeBottom.onload = draw;
